a0aee5fe90a3a8ebf739a4b42af0fe657a11204e
01BARRETO1 <karbar_1990@hotmail.com>
2026-06-25 00:42:17 -0500

    Mover a la izquierda

--- limones.js
+++ limones.js
@@ -5,6 +5,8 @@ const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHURA_PERSONAJE=30;
 
+let personajeX=canvas.width/2
+
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
@@ -17,5 +19,20 @@ function dibujarSuelo(){
 
 function dibujarPersonaje(){
     ctx.fillStyle="#0729be";
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHURA_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHURA_PERSONAJE,ALTURA_PERSONAJE);
+}
+
+function moverIzquierda(){
+    personajeX=personajeX-10;
+    actualizarPantalla();
+}
+//moverDerecha
+function actualizarPantalla(){
+    limpiarCanva();
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
 }
